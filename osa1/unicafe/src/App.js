9fbc783f5d58a705e0776e85434991ddd0723b10
Harri Nieminen <moiman@posteo.net>
2023-03-16 10:47:08 +0200

    osa1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,20 @@
 import { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  return (
+    <div>
+      <h2>tilastot</h2>
+      <p>hyvä {good}</p>
+      <p>neutraali {neutral}</p>
+      <p>huono {bad}</p>
+      <p>kaikki {all}</p>
+      <p>keskiarvo {(good - bad) / (all)}</p>
+      <p>positiivinen {100 * good / (all)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -12,13 +27,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>hyvä</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutraali</button>
       <button onClick={() => setBad(bad + 1)}>huono</button>
-      <h2>tilastot</h2>
-      <p>hyvä {good}</p>
-      <p>neutraali {neutral}</p>
-      <p>huono {bad}</p>
-      <p>kaikki {good + neutral + bad}</p>
-      <p>keskiarvo {(good - bad) / (good + neutral + bad)}</p>
-      <p>positiivinen {100 * good / (good + neutral + bad)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
