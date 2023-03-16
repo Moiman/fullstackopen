c92b73166849917b9173c593399fb4043d6a80f9
Harri Nieminen <moiman@posteo.net>
2023-03-16 10:34:18 +0200

    osa1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>anna palautetta</h1>
+      <button onClick={() => setGood(good + 1)}>hyvä</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutraali</button>
+      <button onClick={() => setBad(bad + 1)}>huono</button>
+      <h2>tilastot</h2>
+      <p>hyvä {good}</p>
+      <p>neutraali {neutral}</p>
+      <p>huono {bad}</p>
+    </div>
+  )
+}
+
+export default App;
