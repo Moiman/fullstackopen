1685bc17cac948fc624989cc4bbd851fd0e1cd26
Harri Nieminen <moiman@posteo.net>
2023-03-19 18:23:11 +0200

    osa2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Filter = ({ newFilter, handleFilterChange }) => {
   return (
@@ -24,7 +25,7 @@ const PersonForm = ({ addPerson, newName, newNumber, handlePersonChange, handleN
   )
 }
 
-const Persons = ({ persons, newFilter}) => {
+const Persons = ({ persons, newFilter }) => {
   return (
     <div>
       {persons.filter(
@@ -37,16 +38,16 @@ const Persons = ({ persons, newFilter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
